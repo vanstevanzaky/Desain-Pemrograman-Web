8575a8f989a59f60ae49155df84aec92edc447cb
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-19 07:54:36 +0700

    Prak.  Bag. 3 Soal No. 3.1

---
+++ dasarWeb2/operator.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat  = $a ** $b;
+?>
+<?php
+echo"Hasil Tambah = {$hasilTambah}<br>";
+echo "Hasil Kurang = {$hasilKurang}<br>";
+echo "hasil Kali = {$hasilKali}<br>";
+echo"Hasil Bagi = {$hasilBagi}<br>";
+echo "Sisa Bagi = {$sisaBagi}<br>";
+echo "Pangkat = {$pangkat}<br>";
+?>
