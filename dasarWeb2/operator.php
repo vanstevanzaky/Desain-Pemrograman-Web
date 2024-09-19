36dd0b5a7e505f3743b09af028c2e0f477f2a850
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-19 08:53:11 +0700

    Prak. Bag. 3 Soal No. 3.2

--- dasarWeb2/operator.php
+++ dasarWeb2/operator.php
@@ -8,6 +8,13 @@ $hasilKali = $a * $b;
 $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat  = $a ** $b;
+
+$hasilSama = $a ==  $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama =  $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
 ?>
 <?php
 echo"Hasil Tambah = {$hasilTambah}<br>";
@@ -16,4 +23,11 @@ echo "hasil Kali = {$hasilKali}<br>";
 echo"Hasil Bagi = {$hasilBagi}<br>";
 echo "Sisa Bagi = {$sisaBagi}<br>";
 echo "Pangkat = {$pangkat}<br>";
+
+echo "Hasil Sama = {$hasilSama}<br>";
+echo "Hasil Tidak Sama = {$hasilTidakSama}<br>";
+echo "Hasil Lebih Kecil = {$hasilLebihKecil}<br>";
+echo "Hasil Lebih Besar = {$hasilLebihBesar}<br>";
+echo "Hasil Lebih Kecil Sama = {$hasilLebihKecilSama}<br>";
+echo "Hasil Lebih Besar Sama = {$hasilLebihBesarSama}<br>";
 ?>
