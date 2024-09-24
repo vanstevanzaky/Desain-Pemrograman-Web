f2d794602b7e844ebc2a35424bf8fbd8358e6273
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-24 14:05:04 +0700

    Praktikum Bag. 3 Soal No. 3.4

--- dasarWeb2/operator.php
+++ dasarWeb2/operator.php
@@ -20,6 +20,13 @@ $hasilAnd =  $a && $b;
 $hasilOr = $a || $b;
 $hasilNotA = !$a;
 $hasilNotB = !$b;
+
+$hasilTmbhPngsn = $a += $b;
+$hasilKrgPngsn = $a -= $b;
+$hasilKliPngsn = $a *= $b;
+$hasilBgPngsn = $a /= $b;
+$hasilSsBgPngsn = $a %= $b;
+
 ?>
 <?php
 echo"Hasil Tambah = {$hasilTambah}<br>";
@@ -40,5 +47,11 @@ echo "Hasil And = {$hasilAnd}<br>";
 echo "Hasil Or = {$hasilOr}<br>";
 echo "Hasil Not A = {$hasilNotA}<br>";
 echo "Hasil Not B = {$hasilNotB}<br>";
+
+echo "Hasil Tambah Penugasan (a += b) = {$hasilTmbhPngsn}<br>";
+echo "Hasil Kurang Penugasan (a -= b) = {$hasilKrgPngsn}<br>";
+echo "Hasil Kali Penugasan (a *= b) = {$hasilKliPngsn}<br>";
+echo "Hasil Bagi Penugasan (a /= b) = {$hasilBgPngsn}<br>";
+echo "Hasil Sisa Bagi Penugasan (a %= b) = {$hasilSsBgPngsn}<br>";
 
 ?>
