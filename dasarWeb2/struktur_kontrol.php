bd99c22d0f83a2020298f7628ac7c8415b1fee2c
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-25 21:06:58 +0700

    Prak. Bag. 4 Soal No. 4.3

--- dasarWeb2/struktur_kontrol.php
+++ dasarWeb2/struktur_kontrol.php
@@ -22,5 +22,15 @@ while ($jarakSaatIni < $jarakTarget) {
     $hari++;
 }
 
-echo"Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer. ";
-?>
+echo"Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer. <br>";
+
+$jumlahLahan = 10;
+$tanamanPerlahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i=1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerlahan * $buahPerTanaman);
+}
+echo "Jumlah buah yang akan dipanen adalah:  $jumlahBuah";
+?>;
