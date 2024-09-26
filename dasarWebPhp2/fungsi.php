404480bb896d842707b7623799a3c336d6a2cd15
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-26 08:23:10 +0700

    PHP2 Soal No.1

---
+++ dasarWebPhp2/fungsi.php
@@ -0,0 +1,9 @@
+<?php
+
+function perkenalan(){
+    echo"Assalamualaikum, ";
+    echo"Perkenalan, nama saya Stevan Zaky Setyanto <br/>";
+    echo "Senang berekenalan dengan Anda<br/>";
+}
+perkenalan();
+perkenalan();                                                                                                                                                                                                                                                                                                                                
