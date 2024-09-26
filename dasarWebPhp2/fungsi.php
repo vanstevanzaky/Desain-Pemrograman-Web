ecb75905eda001aecb2c626b2c9738d95ac8dd84
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-26 09:16:01 +0700

    PHP Soal No. 2

--- dasarWebPhp2/fungsi.php
+++ dasarWebPhp2/fungsi.php
@@ -1,9 +1,29 @@
 <?php
 
-function perkenalan(){
+function perkenalan0(){
     echo"Assalamualaikum, ";
     echo"Perkenalan, nama saya Stevan Zaky Setyanto <br/>";
     echo "Senang berekenalan dengan Anda<br/>";
 }
-perkenalan();
-perkenalan();                                                                                                                                                                                                                                                                                                                                
+perkenalan0();
+perkenalan0();
+echo"<br>";
+echo "<br>";
+
+?>
+
+<?php
+function perkenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ". $nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan("Hamdana","Hallo");
+echo"<br>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
+?>
