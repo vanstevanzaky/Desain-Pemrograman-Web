ca99c18aa8a45cfccd08e48f7b5d9c557839c198
vanstevanzaky <stevanzaky87@gmail.com>
2024-09-28 19:56:56 +0700

    PHP2 Soal No. 8

--- dasarWebPhp2/menu.php
+++ dasarWebPhp2/menu.php
@@ -33,12 +33,16 @@ $menu = [
     ],
     ];
 
-function tampilkanMenuBertingkat (array $menu) {
-    echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
+    function tampilkanMenuBertingkat(array $menu) {
+        echo "<ul>";
+        foreach ($menu as $item) {
+            echo "<li>{$item['nama']}";
+            if (isset($item['subMenu'])) {
+                tampilkanMenuBertingkat($item['subMenu']);
+            }
+            echo "</li>";
+        }
+        echo "</ul>";
     }
-    echo "</li>";
-}
-
-tampilkanMenuBertingkat($menu);
+    
+    tampilkanMenuBertingkat($menu);
