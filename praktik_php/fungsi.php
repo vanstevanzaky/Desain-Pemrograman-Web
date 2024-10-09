40a734a26cc45b335a5250779f7e39df3ca892a0
vanstevanzaky <stevanzaky87@gmail.com>
2024-10-09 21:25:02 +0700

    PHPArray Soal No. 5

--- praktik_php/fungsi.php
+++ praktik_php/fungsi.php
@@ -6,3 +6,20 @@
 }
 perkenalan();
 perkenalan();
+
+echo "<br>";
+echo "<br>";
+
+function perkenalan1($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ". $nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+
+perkenalan1("Stevan Zaky","Hallo");
+echo"<hr>";
+$saya = "Zaky Stevan";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan1($saya, $ucapanSalam);
