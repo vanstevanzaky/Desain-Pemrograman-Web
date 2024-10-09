ae8e5fb2056274b57c1187b9d55ee130277a1076
vanstevanzaky <stevanzaky87@gmail.com>
2024-10-09 20:45:51 +0700

    PHPArray Prak. 2.1

--- praktik_php/array_php2.php
+++ praktik_php/array_php2.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="styleTable.css"/>    
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width", initial-scale1">
         <title>Informasi Dosen</title>
     </head>
     <body>
-        <?php
-        $Dosen =[
-            'nama' => 'Elok Nur Hamdana',
-            'domisili' => 'Malang',
-            'jenis_kelamin' => 'Perempuan' ];
-        echo "Nama : {$Dosen ['nama']} <br>";
-        echo "Domisili : {$Dosen ['domisili']} <br>";
-        echo"Jenis Kelamin : {$Dosen ['jenis_kelamin']}<br>";
-        ?>
+        <table>
+            <tr>
+                <th>Informasi</th>
+                <th>Keterangan</th>
+            </tr>
+                <?php 
+                    $Dosen =[
+                        'nama' => 'Elok Nur Hamdana',
+                        'domisili' => 'Malang',
+                        'jenis_kelamin' => 'Perempuan' ];
+
+                    echo "<tr><td>Nama</td><td>{$Dosen['nama']}</td></tr>";
+                    echo "<tr><td>Domisili</td><td>{$Dosen['domisili']}</td></tr>";
+                    echo "<tr><td>Jenis Kelamin</td><td>{$Dosen['jenis_kelamin']}</td></tr>";
+                    
+                ?>
+        </table>
     </body>
 </html>
