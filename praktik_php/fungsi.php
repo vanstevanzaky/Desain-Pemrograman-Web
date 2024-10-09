bdd656063c218a1aa877e490d8e99a4729c752c0
vanstevanzaky <stevanzaky87@gmail.com>
2024-10-09 21:11:31 +0700

    PHPArray Fungsi Soal No. 4

---
+++ praktik_php/fungsi.php
@@ -0,0 +1,8 @@
+<?php
+  function perkenalan(){
+    echo"Assalamualaikum, ";
+    echo"Perkenalan, nama saya Stevan Zaky Setyanto <br/>";
+    echo "Senang berekenalan dengan Anda<br/>";
+}
+perkenalan();
+perkenalan();
