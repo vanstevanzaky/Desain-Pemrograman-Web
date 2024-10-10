fa3653504ef16359e2898b91cff9b97ea9338492
vanstevanzaky <stevanzaky87@gmail.com>
2024-10-10 07:55:01 +0700

    PHPArray Soal No. 8

--- praktik_php/fungsi.php
+++ praktik_php/fungsi.php
@@ -46,6 +46,20 @@ function hitungUmur($thn_lahir, $thn_sekarang){
 }
 echo"Umur saya adalah " . hitungUmur(2004, 2024);
 echo "<br>";
+echo "<br>";
+
+function hitungUmur1($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+function perkenalan3($nama, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ". $nama."<br/>";
+    echo "Saya berusia " . hitungUmur(2004, 2024)
+    . " tahun<br>";
+    echo "Senang berkenalan dengan Anda<br>";
+}
+perkenalan3("Stevan");
 
 
 
