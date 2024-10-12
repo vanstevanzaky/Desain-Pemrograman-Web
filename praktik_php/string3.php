06e6f3739c6e5807861450cde77f91bfd147e760
vanstevanzaky <stevanzaky87@gmail.com>
2024-10-12 13:32:47 +0700

    PHPArray Soal No.16

--- praktik_php/string3.php
+++ praktik_php/string3.php
@@ -1,6 +1,13 @@
 <?php
 
 $pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
+
+$pesanPerKata = explode(" ", $pesan);
+
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+
+$pesan = implode(" ", $pesanPerKata);
+
+echo ($pesan) . "<br>";
 
 ?>
