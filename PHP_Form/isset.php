5adb724c7fe53e81ff52064879587038ac695f0c
vanstevanzaky <stevanzaky87@gmail.com>
2024-10-30 19:57:01 +0700

    PHPForm Soal No. 1.2

--- PHP_Form/isset.php
+++ PHP_Form/isset.php
@@ -5,3 +5,9 @@ if (isset($umur) && $umur >= 18) {
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan. ";
 }
+$data = array("nama" => "Stevan",  "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " , $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array. ";
+}
