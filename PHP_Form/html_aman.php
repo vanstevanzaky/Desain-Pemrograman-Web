30e891cf19661abedc542c0236554d530e9a9d36
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-03 10:43:31 +0700

    PHPForm Soal No. 4.2

--- PHP_Form/html_aman.php
+++ PHP_Form/html_aman.php
@@ -2,12 +2,23 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input = $_POST['input'];
     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-    echo "Input yang Anda masukkan: " . $input;
+
+    $email = $_POST['email'];
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Input yang Anda masukkan: " . $input . "<br>";
+        echo "Email yang Anda masukkan: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+    } else {
+        echo "Email yang dimasukkan tidak valid.";
+    }
 } else {
     echo '
     <form method="post" action="">
         <label for="input">Masukkan teks:</label>
-        <input type="text" id="input" name="input">
+        <input type="text" id="input" name="input"><br><br>
+
+        <label for="email">Masukkan email:</label>
+        <input type="email" id="email" name="email"><br><br>
+
         <button type="submit">Kirim</button>
     </form>';
 }
