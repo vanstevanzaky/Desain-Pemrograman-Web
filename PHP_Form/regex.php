d62222d20cde37af2aad545b95d828f008f49a3f
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-04 16:56:10 +0700

    PHPForm Soal No. 5.2 - 5.6

--- PHP_Form/regex.php
+++ PHP_Form/regex.php
@@ -1,8 +1,39 @@
-<?php
-$pattern = '/[a-z]/';
-$text = 'This is a Sample Text.';
-if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
-} else {
-    echo"Tidak ada huruf kecil!";
-}
+<!DOCTYPE html>
+<head>
+    <title>Regex</title>
+</head>
+<body>
+    <?php
+    $pattern = '/[a-z]/';
+    $text = 'This is a Sample Text.';
+    if (preg_match($pattern, $text)) {
+        echo "Huruf kecil ditemukan!";
+    } else {
+        echo "Tidak ada huruf kecil!";
+    }
+    echo "<br>";
+    $pattern = '/[0-9]+/';
+    $text = 'There are 123 apples.';
+    if (preg_match($pattern, $text, $matches)) {
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
+    echo "<br>";
+    $pattern = '/apple/';
+    $replacement = 'banana';
+    $text = 'I like apple pie.';
+    $new_text = preg_replace($pattern, $replacement, $text);
+    echo $new_text;
+
+    echo "<br>";
+    $pattern = '/go{2,3}d/';
+    $text = 'god is goood.';
+    if (preg_match($pattern, $text, $matches)) {
+        echo "Cocokkan: ". $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
+    ?>  
+</body>
+</html>
