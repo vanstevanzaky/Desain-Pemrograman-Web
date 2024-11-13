51c6d820e7f1d917de5883c5ca376a133d99a99c
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-13 15:04:53 +0700

    fix No. 8

--- UploadCookiesSession/form_multiupload.php
+++ UploadCookiesSession/form_multiupload.php
@@ -6,7 +6,7 @@
     <body>
         <h2>Unggah Dokumen</h2>
         <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="files[]" multiple="multiple" accept=".jpg, .jpeg, .png"/>
+            <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx"/>
             <input type="submit" value="Unggah" />
         </form>
     </body>
