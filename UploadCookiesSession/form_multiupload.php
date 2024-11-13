901609a10e68f49158a49bcf02324dd4fef52618
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-13 15:09:15 +0700

    FCUS Soal No. 9

--- UploadCookiesSession/form_multiupload.php
+++ UploadCookiesSession/form_multiupload.php
@@ -6,7 +6,8 @@
     <body>
         <h2>Unggah Dokumen</h2>
         <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-            <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx"/>
+            <input type="file" name="files[]" multiple="multiple" accept=".jpg, .jpeg, .png"/>
+
             <input type="submit" value="Unggah" />
         </form>
     </body>
