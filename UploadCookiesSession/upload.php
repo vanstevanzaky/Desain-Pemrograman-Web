2e9c59c3965ae3156726cc9f2aed966e3b9c8337
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-13 14:33:23 +0700

    FCUS Soal No. 3

--- UploadCookiesSession/upload.php
+++ UploadCookiesSession/upload.php
@@ -1,12 +1,22 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targerdir= "uploads/";
+    $targetdir = "uploads/";
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-    
-    if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targerfile)) {
-        echo "File berhasil diunggah";
-    } else {
-        echo "Gagal mengunggah file.";
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 5*1024*1024;
+
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+            echo "File berhasil diunggah";
+        }
+        else {
+            echo "Gagal menggunggah file.";
+        }
+    }
+    else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
     }
 }
 ?>
